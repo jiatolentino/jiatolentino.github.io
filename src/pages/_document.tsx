4c16c3ac4b71fc4544c1ad528831d69f930b1641
jiatolentino <tolentino.joseireneo@gmail.com>
2025-01-19 00:07:48 +0800

    Fixed lint errors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
           <Main />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <div className="border-l mx-2" style={{ borderColor: 'var(--foreground)' }}></div>
       <main className="relative w-5/6 pl-4 overflow-auto custom-scrollbar font-thin mb-4 leading-snug tracking-tighter" style={{ borderColor: 'var(--foreground)' }}>
         <p className="absolute bottom-0 right-0 text-right">
-          "{randomQuote.quote}" <br/>- {randomQuote.author}  
+          &quot;{randomQuote.quote}&quot; <br/>- {randomQuote.author}  
         </p>
       </main>
     </div>
