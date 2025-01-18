d0674b9fefd2e4b9555b9a4183b1a9785755b2f1
jiatolentino <tolentino.joseireneo@gmail.com>
2025-01-18 12:28:20 +0800

    Added deployment scripts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/jiatolentino.github.io/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/jiatolentino.github.io' : '',
+  output: 'export',
 };
 
 export default nextConfig;
